5a23c5c31f16931414cf13672c095ecd0e87dadf
Kaustubh Oak <kaustubh.oak@atos.net>
2024-08-12 15:43:52 +0530

    updated flask app

--- app.py
+++ app.py
@@ -8,4 +8,5 @@ def home():
     return render_template('index.html', quote=quote)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=8080)
